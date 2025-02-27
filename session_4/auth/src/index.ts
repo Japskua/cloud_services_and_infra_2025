7d0b49d96659ef6d31678fbaef0e04c14695596a
Janne Parkkila <japskua@gmail.com>
2025-02-27 16:05:27 +0200

    Moving files to different places for simplicity's sake.

--- session_4/auth/src/index.ts
+++ session_4/auth/src/index.ts
@@ -3,32 +3,14 @@
 import cors from "@elysiajs/cors";
 import swagger from "@elysiajs/swagger";
 import { Elysia, t } from "elysia";
-import { jwtConfig } from "./jwt";
-import { UserDTO } from "./user.dto";
+import { jwtConfig } from "./config/jwt.config";
+import { UserDTO } from "./models/user.model";
+import { authorizationMiddleware } from "./middleware/authorization";
 
 const app = new Elysia()
     .use(jwtConfig)
     // ℹ️ Designed to append new value to context directly before validation
-    .derive(async ({ headers, jwt_auth }) => {
-        // 1. Extract the 'Authorization' header from the incoming request
-        const auth = headers["authorization"];
-
-        // 2. Check if the 'Authorization' header contains a Bearer token
-        //    If it starts with 'Bearer ', extract the token string after 'Bearer '
-        //    Otherwise, set token to null indicating no valid token is present
-        const token = auth && auth.startsWith("Bearer ") ? auth.slice(7) : null;
-
-        // 3. If no token is found, return an object with user set to null
-        if (!token) return { user: null };
-
-        // 4. Verify the JWT token using the jwt_auth module
-        //    This step authenticates the token and retrieves the user information
-        const user = await jwt_auth.verify(token);
-
-        // 5. Return an object containing the authenticated user information
-        //    This will be available inside de request object
-        return { user };
-    })
+    .derive(authorizationMiddleware)
     .use(swagger())
     .use(cors())
     .get("/", () => "Hello from auth!")
--- session_4/auth/src/jwt.ts
+++
@@ -1,8 +0,0 @@
-// auth/src/jwt.ts
-
-import { jwt } from "@elysiajs/jwt";
-
-export const jwtConfig = jwt({
-    name: "jwt_auth", // this name will be used to access jwt within the request object
-    secret: "Super Safe Secret That Should Be In .ENV" // Should be in a .env!!
-});
