692da64bd0d9402faccb97390b568ee89478dc60
Janne Parkkila <japskua@gmail.com>
2025-03-21 17:42:27 +0200

    Fixed csp command [build-nginx]

--- session_6/ui/src/helpers/urlHelper.ts
+++ session_6/ui/src/helpers/urlHelper.ts
@@ -1,5 +1,5 @@
 export const getServiceUrl = (servicePrefix: string): string => {
-    const currentHost = window.location.hostname; // e.g., ui.localhost or ui.myservice.com
+    const currentHost = window.location.hostname; // e.g., app.localhost or app.myservice.com
     const serviceHost = currentHost.replace(/^app\./, `${servicePrefix}.`);
     return `https://${serviceHost}`;
 };
